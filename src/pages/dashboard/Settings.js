b4e931b85757b3a1a10a2433831d0d15e2588ba3
viren1610 <virenplays@gmail.com>
2023-09-07 12:22:24 +0530

    made keyboard shortcuts

--- src/pages/dashboard/Settings.js
+++ src/pages/dashboard/Settings.js
@@ -1,12 +1,38 @@
 import React from "react";
-import { Stack, Box, IconButton, Typography, Avatar } from "@mui/material";
+import {
+  Stack,
+  Box,
+  IconButton,
+  Typography,
+  Avatar,
+  Divider,
+} from "@mui/material";
 import { useTheme } from "@mui/material";
-import { ArrowLeft, Image, Info, Key, Keyboard, Lock, Note, PencilCircle } from "phosphor-react";
+import {
+  ArrowLeft,
+  Image,
+  Info,
+  Key,
+  Keyboard,
+  Lock,
+  Note,
+  PencilCircle,
+} from "phosphor-react";
 import { faker } from "@faker-js/faker";
 import { Bell } from "phosphor-react";
-const Settings = () => {
-  const theme = useTheme();
+import Shortcuts from "../../sections/settings/Shortcuts";
+import { useState } from "react";
 
+const Settings = () => {
+  const [openShortcuts, setShortcuts] = useState(false);
+
+  function handleOpenShortcuts() {
+    setShortcuts(true);
+  }
+  function handleCloseShortcuts() {
+    setShortcuts(false);
+  }
+  const theme = useTheme();
   const List = [
     {
       key: 0,
@@ -48,8 +74,7 @@ const Settings = () => {
       key: 6,
       icon: <Keyboard size={20} />,
       title: "KeyBoard Shortcuts",
-      //onClick: handleOpenShortcuts,
-      onClick:()=>{},
+      onClick: handleOpenShortcuts,
     },
     {
       key: 7,
@@ -59,46 +84,66 @@ const Settings = () => {
     },
   ];
   return (
-    <Stack direction={"row"} width={"100%"}>
-      {/*Chota Box */}
-      <Box
-        sx={{
-          width: 320,
-          height: "100vh",
-          overflowY: "scroll",
-          backgroundColor:
-            theme.palette.mode === "light"
-              ? "#F8FAFF"
-              : theme.palette.background,
-          boxShadow: "0 0 2 rgba(0,0,0,0.25)",
-        }}
-      >
-        {/* All the contents in column */}
-        <Stack direction={"column"} p={3} spacing={5}>
-          {/*Header */}
-          <Stack direction={"row"} alignItems={"center"} spacing={3}>
-            <IconButton>
-              <ArrowLeft sx={24} />
-            </IconButton>
-            <Typography variant="h6">Settings</Typography>
-          </Stack>
-          {/*Avatar */}
-          <Stack direction={"row"} spacing={4}>
-            <Avatar
-              src={faker.image.avatar()}
-              alt={faker.name.fullName()}
-              sx={{ height: 56, width: 56 }}
-            />
-            <Stack direction={"column"} spacing={0.5}>
-              <Typography variant="article">{faker.name.fullName()}</Typography>
-              <Typography variant="body2">{faker.random.words()}</Typography>
+    <>
+      <Stack direction={"row"} width={"100%"}>
+        {/*Chota Box */}
+        <Box
+          sx={{
+            width: 320,
+            height: "100vh",
+            overflowY: "scroll",
+            backgroundColor:
+              theme.palette.mode === "light"
+                ? "#F8FAFF"
+                : theme.palette.background,
+            boxShadow: "0 0 2 rgba(0,0,0,0.25)",
+          }}
+        >
+          {/* All the contents in column */}
+          <Stack direction={"column"} p={3} spacing={5}>
+            {/*Header */}
+            <Stack direction={"row"} alignItems={"center"} spacing={3}>
+              <IconButton>
+                <ArrowLeft sx={24} />
+              </IconButton>
+              <Typography variant="h6">Settings</Typography>
+            </Stack>
+            {/*Avatar */}
+            <Stack direction={"row"} spacing={4}>
+              <Avatar
+                src={faker.image.avatar()}
+                alt={faker.name.fullName()}
+                sx={{ height: 56, width: 56 }}
+              />
+              <Stack direction={"column"} spacing={0.5}>
+                <Typography variant="article">
+                  {faker.name.fullName()}
+                </Typography>
+                <Typography variant="body2">{faker.random.words()}</Typography>
+              </Stack>
+            </Stack>
+            {/*List of Options*/}
+            <Stack spacing={4}>
+              {List.map(({ key, icon, title, onClick }) => (
+                <Stack sx={{ cursor: "pointer" }} spacing={2} onClick={onClick}>
+                  <Stack direction={"row"} spacing={2} alignItems={"center"}>
+                    {icon}
+
+                    <Typography variant="caption2">{title}</Typography>
+                  </Stack>
+                  {key !== 7 && <Divider />}
+                </Stack>
+              ))}
             </Stack>
           </Stack>
-          {/*List of Options*/}
-          
-        </Stack>
-      </Box>
-    </Stack>
+        </Box>
+
+        {/*Right Side vala pannel */}
+      </Stack>
+      {openShortcuts && (
+        <Shortcuts open={openShortcuts} handleClose={handleCloseShortcuts} />
+      )}
+    </>
   );
 };
 
